57a9f7926c0f3e80fe627d39d7232d70c18f5d08
sukhbat <axefirm1gmail.com>
2019-11-30 12:17:59 +0800

    login auth

---
+++ frontend/pages/secret.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+    Logged succesfully
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+  async asyncData({$axios, store}){
+    console.log(store.state.token);
+    $axios.setToken(store.state.token);
+    let res = await $axios.$get("http://localhost:8080/info")
+    return {res: res};
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
